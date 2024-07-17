e10ccf328e2c937455163e11ab17851d7a3170cb
Songklod Saengvoratip <songklod@marvelous.studio>
2024-07-17 23:16:17 +0700

    feat: move banks to data

---
+++ data/banks.js
@@ -0,0 +1,92 @@
+export const banks = [
+  {
+    //0
+    logo: "/bank/Dime.png",
+    name: "Dime! Save",
+    bank: "เกียรตินาคิน",
+    saving: 0,
+    ratio: 0,
+    interest: 3 / 100,
+  },
+  {
+    //1
+    logo: "/bank/TTB-ME-Save.png",
+    name: "TTB ME Save",
+    bank: "ธนาคารทหารไทยธนชาต",
+    saving: 0,
+    ratio: 0,
+    interest: 2.2 / 100,
+  },
+  {
+    //2
+    logo: "/bank/KKP-Start-Saving.png",
+    name: "KKP Start Saving",
+    bank: "ธนาคารเกียรตินาคินภัทร",
+    saving: 0,
+    ratio: 0,
+    interest: 2.2 / 100,
+  },
+  {
+    //3
+    logo: "/bank/Chill-D.png",
+    name: "CIMB Chill-D",
+    bank: "ธนาคาร ซีไอเอ็มบี ไทย",
+    saving: 0,
+    ratio: 0,
+    interest: 2.21 / 100,
+  },
+  {
+    //4
+    logo: "/bank/KKP-Start-Saving.png",
+    name: "KKP Start Saving",
+    bank: "ธนาคารเกียรตินาคินภัทร",
+    saving: 0,
+    ratio: 0,
+    interest: 2 / 100,
+  },
+  {
+    //5
+    logo: "/bank/Alpha-Saving.png",
+    name: "Alpha Saving",
+    bank: "ธนาคารไทยเครดิต",
+    saving: 0,
+    ratio: 0,
+    interest: 2 / 100,
+  },
+  {
+    //6
+    logo: "/bank/Kept.png",
+    name: "Kept",
+    bank: "ธนาคารกรุงศรีอยุธยา",
+    saving: 0,
+    ratio: 0,
+    interest: 1.87 / 100,
+  },
+  {
+    //7
+    logo: "/bank/LH-B-You Wealth.png",
+    name: "LH B-You Wealth",
+    bank: "ธนาคารแลนด์ แอนด์ เฮ้าส์",
+    saving: 0,
+    ratio: 0,
+    interest: 1.87 / 100,
+  },
+  {
+    //8
+    logo: "/bank/TTB-ME-Save.png",
+    name: "TTB ME Save",
+    bank: "ธนาคารทหารไทยธนชาต",
+    saving: 0,
+    ratio: 0,
+    interest: 1.6 / 100,
+  },
+  {
+    //9
+    logo: "/bank/CIMB-Speed D+.png",
+    name: "CIMB Speed D+",
+    bank: "ธนาคาร ซีไอเอ็มบี ไทย",
+    saving: 0,
+    ratio: 0,
+    interest: 1.66 / 100,
+  },
+];
