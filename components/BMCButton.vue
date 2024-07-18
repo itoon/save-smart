8fad3a874838ba160272139fe13e3bf93bfd0845
Lostjerome <naronkrach@gmail.com>
2024-07-18 22:49:28 +0700

    Redesign footer and "Buy Me a Coffee" button

---
+++ components/BMCButton.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="fixed right-4 bottom-4">
+    <a
+      href="https://www.buymeacoffee.com/songklodS"
+      target="_blank"
+      class="inline-flex p-2 bg-yellow-400 rounded-full"
+    >
+    <IconCoffee class="fill-black" />
+    </a>
+  </div>
+</template>
