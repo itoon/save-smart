44e2d429e39f73ddd15aec4fb14ee6092b5bded3
Lostjerome <naronkrach@gmail.com>
2024-07-18 17:33:04 +0700

    Refactor

---
+++ components/BankItem.vue
@@ -0,0 +1,43 @@
+<template v-for="(summary, index) in bankCalculator">
+	<div
+		v-if="index != 4 && (summary.saving > 0 || isShowInput)"
+		class="border border-[#DFDFDF] p-4 bg-white rounded-lg flex gap-3"
+	>
+		<div>
+			<img :src="summary.logo" class="w-10 h-10 rounded-lg" />
+		</div>
+		<div class="flex justify-between flex-1">
+			<div class="flex flex-col gap-0.5">
+				<p class="text-sm font-bold">{{ summary.name }}</p>
+				<p class="text-[#A1A1A1] text-xs">{{ summary.bank }}</p>
+			</div>
+			<div class="flex flex-col gap-0.5">
+				<p class="text-sm font-bold text-right">
+					{{
+						Intl.NumberFormat("th-TH", {
+							style: "currency",
+							currency: "THB",
+						}).format(summary.saving)
+					}}
+				</p>
+				<p class="text-[#A1A1A1] text-xs text-right">
+					สัดส่วน {{ summary.ratio.toFixed(2) }}%
+				</p>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script setup lang="ts">
+defineProps<{ 
+	summary: {
+		logo: string;
+		name: string;
+		bank: string;
+		saving: number;
+		ratio: number;
+	};
+	index: number;
+	isShowInput: boolean;
+}>()
+</script>
