6474dd88b9e77fb4eec7158cec82191350fe90a7
Lostjerome <naronkrach@gmail.com>
2024-07-18 23:16:22 +0700

    Improve for responsive views

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,29 +1,31 @@
 <template>
-  <footer class="container flex gap-4 p-8 mx-auto mt-16 text-center bg-[#2969FF] border-t">
-    <a
-      href="https://www.facebook.com/permalink.php?story_fbid=pfbid07qpqzy8TA8s2qUi1kPYqYZdhsjct2LjJRbb16JRhbt8dnHWsqhAeVxDffskZ538Yl&id=61558121405445&rdid=pc5pJgOCanjpDqzx"
-      target="_blank"
-      class="bg-[#2969FF] flex items-center justify-center leading-8 w-full border border-gray-300 rounded-2xl aspect-square text-white hover:bg-blue-600 duration-200 p-8">
-      <p>อ่านโพสต์ต้นฉบับของ
-        <br/>
-        <span class="inline-flex items-center gap-1 text-2xl font-bold">
-          <IconFacebook /> เนิร์ดไฟแนนซ์
-        </span>
-      </p>
-    </a>
-    <a 
-      href="https://www.youtube.com/@codetraveler9432"
-      target="_blank"
-      class="flex items-center text-white justify-center w-full gap-1 p-8 duration-200 bg-[#2969FF] border border-gray-300 rounded-2xl aspect-square hover:bg-blue-600">
-      <p>
-        Developed by 
-        <br/>
-        <span class="inline-flex items-center gap-1 text-2xl font-bold">
-          <img src="../public/CodeTraveler.png" class="w-5 h-5" />
-          CodeTraveler
-        </span>
-      </p>
-    </a>
-    <BMCButton />
+  <footer class="px-8 py-12 mt-16 text-center bg-[#2969FF]">
+    <div class="container flex flex-col items-center gap-4 mx-auto sm:justify-end sm:flex-row">
+      <a
+        href="https://www.facebook.com/permalink.php?story_fbid=pfbid07qpqzy8TA8s2qUi1kPYqYZdhsjct2LjJRbb16JRhbt8dnHWsqhAeVxDffskZ538Yl&id=61558121405445&rdid=pc5pJgOCanjpDqzx"
+        target="_blank"
+        class="bg-[#2969FF] flex items-center max-w-xs justify-center leading-8 w-full border border-gray-300 rounded-2xl aspect-square text-white hover:bg-blue-600 duration-200 p-4">
+        <p>อ่านโพสต์ต้นฉบับของ
+          <br/>
+          <span class="inline-flex items-center gap-1 text-2xl font-bold">
+            <IconFacebook /> เนิร์ดไฟแนนซ์
+          </span>
+        </p>
+      </a>
+      <a 
+        href="https://www.youtube.com/@codetraveler9432"
+        target="_blank"
+        class="flex items-center text-white max-w-xs justify-center w-full gap-1 p-4 duration-200 bg-[#2969FF] border border-gray-300 rounded-2xl aspect-square hover:bg-blue-600">
+        <p>
+          Developed by 
+          <br/>
+          <span class="inline-flex items-center gap-1 text-2xl font-bold">
+            <img src="../public/CodeTraveler.png" class="w-5 h-5" />
+            CodeTraveler
+          </span>
+        </p>
+      </a>
+      <BMCButton />
+    </div>
   </footer>
 </template>
